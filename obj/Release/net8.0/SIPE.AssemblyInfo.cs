24c2e4eaa29b507b2451741e7829bce8ee66ad4f
ClaudioDiaz <gotenyayo09@gmail.com>
2024-11-26 19:26:49 -0800

    Prueba para cargar en git

--- obj/Release/net8.0/SIPE.AssemblyInfo.cs
+++ obj/Release/net8.0/SIPE.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIPE")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7031dc110bbddc0c1d6db505ad55f5ba894e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIPE")]
--- obj/Release/net8.0/SIPE.RazorAssemblyInfo.cs
+++ obj/Release/net8.0/SIPE.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SIPE")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7031dc110bbddc0c1d6db505ad55f5ba894e6a")]
-[assembly: System.Reflection.AssemblyProductAttribute("SIPE")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SIPE")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
