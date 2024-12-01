fbc5406878471c4c612e9fff515f52e16cebf2d1
Oscar-Ismael <oscar.flores58@uabc.edu.mx>
2024-12-01 13:32:06 -0800

    Se agrego bien la imagen de SIPE sin el fondo blanco
    En el header de login ya tiene el logo de SIPE en blanco

--- obj/Debug/net8.0/SIPE.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIPE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIPE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557fd8f7424a8c415d4aa7a43a4267499a121887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5314786eef9927a20920bfabad1c0a9f391e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIPE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIPE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
